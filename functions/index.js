bd2cf0c56f3fca5d52a029c7351ef46cdc4070f7
Ricardo Tan <ricardo.tan.2000@gmail.com>
2024-05-24 11:56:42 +0700

    added logger

--- functions/index.js
+++ functions/index.js
@@ -19,19 +19,64 @@ const logger = require("firebase-functions/logger");
 // });
 
 const axios = require('axios');
-const cors = require('cors')({ origin: true });
+const cors = require('cors')({origin: true});
+const http = require('http');
+const https = require('https');
 
-exports.proxyApi = onRequest(async (req, res) => {
+const httpAgent = new http.Agent({keepAlive: true});
+const httpsAgent = new https.Agent({keepAlive: true});
+
+exports.proxyApi = onRequest({
+  region: 'asia-southeast2',
+}, async (req, res) => {
   cors(req, res, async () => {
     try {
+      const {host, ...restHeaders} = req.headers;
+
+      logger.info('Incoming request', {
+        method: req.method,
+        url: req.url,
+        headers: req.headers,
+        body: req.body
+      });
+
       const response = await axios({
         method: req.method,
         url: 'http://34.128.120.186' + req.url,
         data: req.body,
-        headers: req.headers
+        headers: {
+          ...restHeaders,
+        },
+        httpAgent: httpAgent,
+        httpsAgent: httpsAgent,
+      });
+      logger.info('API response', {
+        status: response.status,
+        data: response.data
       });
       res.status(response.status).send(response.data);
     } catch (error) {
+      if (error.code === 'ECONNABORTED' || error.code === 'ECONNRESET') {
+        logger.error('Network error', {
+          message: error.message,
+          code: error.code,
+          config: error.config
+        });
+      } else if (error.response) {
+        logger.error('Request failed', {
+          message: error.message,
+          response: error.response.data,
+          status: error.response.status,
+          headers: req.headers
+        });
+      } else {
+        logger.error('Error', {
+          message: error.message,
+          code: error.code,
+          stack: error.stack
+        });
+      }
+
       const status = error.response ? error.response.status : 500;
       const data = error.response ? error.response.data : 'Internal Server Error';
       res.status(status).json(data);
